5115dd61223f732dd949d3f7c9cae6347b4c4093
Justin Hong <jhong2009@gmail.com>
2018-03-26 19:42:04 -0700

    set up general logic of setInterval

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -6,8 +6,6 @@ const msHundreds = document.getElementById('msHundreds');
 
 let count = 0;
 
-// 100ms - msTens
-// 10ms - msOnes
 let timer = setInterval(function() {
     // increment count by 10
     count += 10;
@@ -16,10 +14,22 @@ let timer = setInterval(function() {
     if (count === 10000) {
         secondTens.innerHTML = 1;
         secondOnes.innerHTML = 0;
+        msTens.innerHTML = 0;
+        msHundreds.innerHTML = 0;
         clearInterval(timer);
     }
     // 1000ms - secondOnes
-    else if (count >= 1000 && count < 10000) {
-        secondOnes.innerHTML = counter / 1000;
+    else if (count % 1000 === 0) {
+        secondOnes.innerHTML = Number(count.toString()[0]) + 1;
+        msTens.innerHTML = 0;
+    }
+    // 100ms - msTens
+    else if (count % 100 === 0) {
+        msTens.innerHTML = Number(count.toString()[0]) + 1;
+        msHundreds.innerHTML = 0;
+    } 
+    // 100ms - msTens
+    else if (count % 10 === 0) {
+        msHundreds.innerHTML = Number(count.toString()[0]) + 1;
     }
 }, 10);

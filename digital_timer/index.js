2e3bd6d593741584d268eee3046072ccf687b19b
Justin Hong <jhong2009@gmail.com>
2018-03-26 19:13:39 -0700

    set up count var and a setInterval fx for 10mx

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,3 +3,14 @@ const secondTens = document.getElementById('secondTens');
 const secondOnes = document.getElementById('secondOnes');
 const msTens = document.getElementById('msTens');
 const msHundreds = document.getElementById('msHundreds');
+
+let count = 0;
+// 1000ms - secondOnes
+// 100ms - msTens
+// 10ms - msOnes
+let timer = setInterval(function() {
+    // 10000ms - secondTens
+    if (count === 10000) {
+        clearInterval(timer);
+    }
+}, 10);

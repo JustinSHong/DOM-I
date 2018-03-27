2204540cb0e0879bcc0048b9bfdae513fdf99e50
Justin Hong <jhong2009@gmail.com>
2018-03-27 15:33:56 -0700

    set up progress fx and set up if for when ms is 10seconds

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -6,15 +6,15 @@ const msTens = document.getElementById('msTens');
 const msHundreds = document.getElementById('msHundreds');
 
 // initialize digits to 0
-secondOnes.innerHTML = "0";
-secondTens.innerHTML = "0";
-msTens.innerHTML = "0";
-msHundreds.innerHTML = "0";
+secondOnes.innerHTML = '0';
+secondTens.innerHTML = '0';
+msTens.innerHTML = '0';
+msHundreds.innerHTML = '0';
 
 // keeps track of time in milliseconds
 let ms = 0
 
-let timer = setInterval(function() {
+const timer = setInterval(function() {
 	// increment ms by 10 for accuracy
 	ms += 10;
 	// our limit is 10seconds or 10000ms
@@ -27,3 +27,12 @@ let timer = setInterval(function() {
 		progress();
 	}
 }, 10);
+
+const progress = (ms) => {
+	if (ms === 10000) {
+		secondTens.innerHTML = '1';
+		secondOnes.innerHTML = '0';
+		msHundreds.innerHTML = '0';
+		msTens.innerHTML = '0';
+	}
+}

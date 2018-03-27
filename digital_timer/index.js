18458b1b071707dd6703cef9ea4f9c0dcbdb72aa
Justin Hong <jhong2009@gmail.com>
2018-03-26 19:09:25 -0700

    set up html doc and variables in index.js

---
+++ digital_timer/index.js
@@ -0,0 +1,5 @@
+const digits = document.querySelector('.digits');
+const secondTens = document.getElementById('secondTens');
+const secondOnes = document.getElementById('secondOnes');
+const msTens = document.getElementById('msTens');
+const msHundreds = document.getElementById('msHundreds');

6f29d6a50f743b80ac38ac480003c44cda163d6d
Justin Hong <jhong2009@gmail.com>
2018-03-27 15:19:30 -0700

    initialize var ms to 0 and reset setInterval

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -11,34 +11,9 @@ secondTens.innerHTML = "0";
 msTens.innerHTML = "0";
 msHundreds.innerHTML = "0";
 
-let count = 0;
+// keeps track of time in milliseconds
+let ms = 0
 
 let timer = setInterval(function() {
-    // increment count by 10
-    count += 10;
-
-    // when count reaches 10000ms
-    if (count === 10000) {
-        secondTens.innerHTMl = '1';
-        secondOnes.innerHTMl = '0';
-        msTens.innerHTMl = '0';
-        msHundreds.innerHTMl = '0';
-        clearInterval(timer);
-    }
-    // when 1000 < counter < 10000
-    else if (count % 1000 === 0) {
-        secondOnes.innerHTMl = (Number(secondOnes.innerHTML) + 1).toString();
-        msTens.innerHTMl = '0';
-        msHundreds.innerHTMl = '0';
-    }
-    // when 100 < counter < 1000
-    else if (count % 100 === 0) {
-        msTens.innerHTMl = (Number(msTens.innerHTML) + 1).toString();
-        msHundreds.innerHTMl = '0';
-    }
-    // when count < 100
-    else {
-        msHundreds.innerHTMl = (Number(msHundreds.innerHTML) + 1).toString();
-    }
 
 }, 10);

84ccbdb78ddccd11af3f11e05a2b0d398b846d28
Justin Hong <jhong2009@gmail.com>
2018-03-26 19:17:19 -0700

    set stop case and set timer to run at secondOnes

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -5,12 +5,21 @@ const msTens = document.getElementById('msTens');
 const msHundreds = document.getElementById('msHundreds');
 
 let count = 0;
-// 1000ms - secondOnes
+
 // 100ms - msTens
 // 10ms - msOnes
 let timer = setInterval(function() {
+    // increment count by 10
+    count += 10;
     // 10000ms - secondTens
+        // stop the timer
     if (count === 10000) {
+        secondTens.innerHTML = 1;
+        secondOnes.innerHTML = 0;
         clearInterval(timer);
+    }
+    // 1000ms - secondOnes
+    else if (count >= 1000 && count < 10000) {
+        secondOnes.innerHTML = counter / 1000;
     }
 }, 10);
